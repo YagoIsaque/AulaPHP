0ef0e382beef54b115e6c15e9d23e6b08c3347cf
Yago Isaque <yago.soares@etec.sp.gov.br>
2025-06-17 14:02:46 -0300

    1706

---
+++ aula1/exemplo.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>  Aula 1 - Exemplo 1</h1>
+
+    <?php
+    $nome = "Yago Isaque";
+    $idade = 16;
+
+    echo "Olá $nome, você tem $idade anos! ";
+    ?>
+</body>
+</html>
